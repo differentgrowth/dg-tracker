1e28ec3bb94329e5be56a58f2fbe9d166b3074e5
Alejandro Romero <admin@differentgrowth.com>
2026-05-06 12:01:51 +0200

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,31 @@
-import { Geist, Geist_Mono } from "next/font/google"
+import { Geist_Mono, Urbanist } from "next/font/google";
 
-import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'})
+import "./globals.css";
+
+const fontSans = Urbanist({ subsets: ["latin"], variable: "--font-sans" });
 
 const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-})
+	subsets: ["latin"],
+	variable: "--font-mono",
+});
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode
+	children: React.ReactNode;
 }>) {
-  return (
-    <html
-      lang="en"
-      suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", geist.variable)}
-    >
-      <body>
-        <ThemeProvider>{children}</ThemeProvider>
-      </body>
-    </html>
-  )
+	return (
+		<html
+			lang="en"
+			suppressHydrationWarning
+			className={cn("antialiased", fontMono.variable, fontSans.variable)}
+		>
+			<body>
+				<ThemeProvider>{children}</ThemeProvider>
+			</body>
+		</html>
+	);
 }
